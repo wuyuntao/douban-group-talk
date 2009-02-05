0c31f0b6aac947fb6757379e8b9a6dd773bcda67
wu yuntao <wyt.py@thonic.org>
2009-02-05 22:58:23 +0800

    Add douban_talk.user.js

---
+++ douban_talk.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name           Douban Talk
+// @namespace      http://blog.luliban.com
+// @include        http://www.douban.com/group/
+// ==/UserScript==
+
+if (typeof unsafeWindow.jQuery != 'undefined') {
+    var $ = jQuery = unsafeWindow.jQuery;
+}
+
+$(function() {
+
+});

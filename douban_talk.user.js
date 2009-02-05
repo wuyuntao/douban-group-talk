7afff8058738308e41b4aa85373cd264ed890da1
wu yuntao <wyt.py@thonic.org>
2009-02-05 23:18:51 +0800

    Popup plugin structure

--- douban_talk.user.js
+++ douban_talk.user.js
@@ -4,10 +4,68 @@
 // @include        http://www.douban.com/group/
 // ==/UserScript==
 
-if (typeof unsafeWindow.jQuery != 'undefined') {
-    var $ = jQuery = unsafeWindow.jQuery;
-}
+// Set jquery
+var $ = jQuery = unsafeWindow.jQuery;
 
+// Main entry
 $(function() {
 
+    if (inPopup()) {
+        $('body').popupStyle();
+    } else {
+        topic.click(function() {
+            talk.popup('toggle');
+        });
+    }
+
 });
+
+// Check if page is in popup
+function inPopup() {
+    return location.href.match(/#douban\-talk\-popup/);
+}
+
+// Popup window
+$.fn.popup = function(action, options) {
+    // Shift arguments if action is ommited
+    if (typeof options == 'undefined') {
+        options = action;
+        action = 'init';
+    }
+
+    // Set options
+    options = $.extend({
+        url: location.href
+    }, options || {});
+
+    // Work by action
+    switch(action) {
+        case 'open': open(); break;
+        case 'close': close(); break;
+        case 'toggle': toggle(); break;
+        case 'init':
+        default: init(); break;
+    }
+
+    // Elements
+    var popup = this;
+    var iframe = this.children('iframe');
+
+    function init() {
+    }
+
+    function open() {
+    }
+
+    function close() {
+    }
+
+    function toggle() {
+    }
+
+    return this;
+};
+
+// Set iframe popup into gtalk style
+$.fn.popupStyle = function() {
+};
